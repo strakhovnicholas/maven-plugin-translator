367ea8e56d08b7e01359a9d7c3be1b771823dfeb
Strakhov Nicholas <strahovnikolaj27@gmail.com>
2025-08-22 13:10:56 +0300

    Протестировал достать код из всех файлов с расширением .java

--- src/main/java/ru/yandex/practicum/TranslatorMOJO.java
+++ src/main/java/ru/yandex/practicum/TranslatorMOJO.java
@@ -3,8 +3,17 @@ package ru.yandex.practicum;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.utils.io.DirectoryScanner;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 @Mojo(name = "translate")
 public class TranslatorMOJO extends AbstractMojo {
@@ -27,8 +36,44 @@ public class TranslatorMOJO extends AbstractMojo {
     @Parameter(defaultValue = "false")
     private boolean translateStrings;
 
+    @Parameter(defaultValue = "**/*.java")
+    private List<String> includes;
+
+    @Parameter
+    private String apiKey;
+
+    @Parameter
+    private List<String> excludes;
+
+    @Component
+    private MavenProject mavenProject;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        DirectoryScanner scanner  = new DirectoryScanner();
+        scanner.setBasedir(mavenProject.getBasedir());
+        scanner.setIncludes(includes.toArray(new String[includes.size()]));
+        scanner.setCaseSensitive(false);
+        scanner.scan();
+        String[] files = scanner.getIncludedFiles();
+        System.out.println(String.join(" , ",files));
 
+        for (String file : files) {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                StringBuilder sb = new StringBuilder();
+                while (br.ready()){
+                    String line = br.readLine();
+                    sb.append(line + "\n");
+                }
+//                Files.createDirectory(Paths.get(mavenProject.getBuild().getDirectory()));
+//                BufferedWriter bf = new BufferedWriter();
+                System.out.println(sb.toString());
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
