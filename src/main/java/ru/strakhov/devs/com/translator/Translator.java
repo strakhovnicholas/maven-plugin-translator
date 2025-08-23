f84b1ad66d1c6f0e71d6fa1377d4f938468942b0
Strakhov Nicholas <strahovnikolaj27@gmail.com>
2025-08-23 13:20:11 +0300

    Интерфейс стал абстрактным классом со final полем log

--- src/main/java/ru/strakhov/devs/com/translator/Translator.java
+++ src/main/java/ru/strakhov/devs/com/translator/Translator.java
@@ -1,8 +1,12 @@
-package ru.strakhov.devs.com.implementation;
+package ru.strakhov.devs.com.translator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.strakhov.devs.com.translator.implementation.LibreTranslateTranslator;
 
 import java.io.IOException;
 
-public interface Translator {
-    String translate(String text, String sourceLang, String targetLang) throws IOException,
-            InterruptedException;
+public abstract class Translator {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+    public abstract String translate(String text, String sourceLang, String targetLang);
 }
